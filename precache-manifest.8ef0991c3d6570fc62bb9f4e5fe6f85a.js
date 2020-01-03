3aaaf658fca81667b9982f61d7fa25dee0543bb0
Guillaume <gthouvenin@kalray.eu>
2020-01-03 10:50:09 +0100

    Updates

---
+++ precache-manifest.8ef0991c3d6570fc62bb9f4e5fe6f85a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ecdc7ec2d6a5c0e4821613d54c5502c",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "6fcc90605d1e6d764de1",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c27d0eb9421998094531",
+    "url": "/react-gh-pages/static/js/2.52b09f7b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.52b09f7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6fcc90605d1e6d764de1",
+    "url": "/react-gh-pages/static/js/main.b1f56885.chunk.js"
+  },
+  {
+    "revision": "4311346c467d0286cebd",
+    "url": "/react-gh-pages/static/js/runtime-main.721d951e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
